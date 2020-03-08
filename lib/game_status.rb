cf4e7e501b495a16488d68a98dda78302708ab51
Roland McRae <mcraeroland@gmail.com>
2020-03-08 05:58:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,5 +16,7 @@ WIN_COMBINATIONS = [
   # ETC, an array for each win combination
 ]
 def won?(board)
-  puts board[WIN_COMBINATIONS[0][2]]
+  WIN_COMBINATIONS.select |combo| do
+    if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X" || board[combo[0]] == "O" &&board[combo[1]] == "O" &&board[combo[2]] == "O"
+  end
 end
