74ba7260e1cfe8b4f6249a2c40f6e8c236b080c3
Roland McRae <mcraeroland@gmail.com>
2020-03-08 06:00:59 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,6 +18,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.select do |combo|
     if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X" || board[combo[0]] == "O" &&board[combo[1]] == "O" &&board[combo[2]] == "O"
+    end
+    return combo
   end
-end
 end
